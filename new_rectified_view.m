35acce45aebafadd0133208de6031c112500bf91
andrethom <41125321+andrethom@users.noreply.github.com>
2018-08-21 21:02:31 +0200

    korrigiert bzw. viele unnötige Rechnungen entfernt

--- new_rectified_view.m
+++ new_rectified_view.m
@@ -1,47 +1,51 @@
-function new_rectified_img = new_rectified_view(I1_rec,depth_map,K,T_x)
+function new_rectified_img = new_rectified_view(I1_rec,depth_map,T_x)
 %NEW_RECTIFIED_VIEW Gibt rektifizierte Zwischenansicht zurück
 % -----------------------------------------------------------------------
 % !!! T_x zwischen 0 (linke Ansicht) und -1 (rechte Ansicht) wählen !!!
 % -----------------------------------------------------------------------
 
 % P2 = R*P1+T mit R=I und P1=lamda1*x1
-% lambda2*x2 = lambda1*x1+T => lambda2 ergibt sich durch Normierung der
-% z-Komponente von P2 auf 1.
-% Analoge Rechnung für Pixelkoordinaten:
-% => lambda2*x2_pixel = lambda1*K*R*inv(K)*x1_pixel+K*T <= term_1
-% P2 = inv(K)*K*lambda2*x2 = inv(K)*lambda2*x2_pixel
-% => lambda2 ergibt sich durch Normierung der
-% z-Komponente von P2 auf 1 => lambda2 = (3. Zeile von inv(K))*term_1 
-% lambda1 ist durch depth_map gegeben, K*R*inv(K) = I und K*T = T_pixel
-% => lambda2*x2_pixel = lambda1*x1_pixel+T_pixel <= term_1
+% lambda2*x2 = lambda1*x1+T => lambda1 und lambda2 sind hier in diesem Fall
+% identisch. (Verwendung von rektifizierten Bildern bzw. eukl. Bewegung
+% lässt z-Komponente unverändert, x1 und x2 auf z=1 normiert)
+% Daher gilt: lambda1 = lambda2 = z-Komponente der Punkte im Raum
+% Letzeres ist gegeben durch die Tiefenkarte/depth_map (Achtung Tiefenkarte
+% ist in Pixeleinheiten)
 
-% T_x soll zwischen 0 und -1 liegen
+% Analoge Rechnung für Pixelkoordinaten:
+% => lambda2*x2_pixel = lambda1*K*R*inv(K)*x1_pixel+K*T
+% mit K*R*inv(K) = I und K*T = T_pixel
+% => lambda2*x2_pixel = lambda1*x1_pixel+T_pixel <= Gleichung(1)
+
 % Nur T_x nötig, da nur Verschiebung in x-Richtung (T = [T_x; 0; 0])
-% T_pixel = K*T = [K(1,1)*T_x; 0; 0]
+% T_x soll zwischen 0 und -1 liegen
 
 [L_pixel_x,L_pixel_y] = meshgrid(1:size(I1_rec,2),1:size(I1_rec,1));
 
-P2_x_pixel = depth_map'.*L_pixel_x+T_x;
-% T(2) = 0 und T(3) = 0, aus leterem folgt, dass P2_z = depth_map'
-P2_y_pixel = depth_map'.*L_pixel_y;
-% lambda2 ergibt sich durch Normierung der z-Komponente von P2 auf 1
-% Skalarprodukt von 3. Zeile von inv(K) mit
-% [P2_x_pixel P2_y_pixel P2_z_pixel]
-K_inv = inv(K);
-lambda2 = K_inv(3,1).*(P2_x_pixel+K(1,1)*T_x) + K_inv(3,2).*P2_y_pixel +...
-          K_inv(3,3).*depth_map'; 
-% Normierung mit lambda2
-NEW_pixel_x = P2_x_pixel./lambda2;
-NEW_pixel_y = P2_y_pixel./lambda2;
+% Tiefe wurde bisher bestimmt als: depth = 1/(x1_pixel-x2_pixel). Der erste
+% Eintragd der Kalibrierungsmatrix K(1,1) entspricht dem Umrechnungsfaktor
+% zwischen Abständen der x-Weltkoordinaten und x-Pixelkoordinaten:
+% d_x_pixel = K(1,1)*d_x_welt
+% Daraus folgt für die "echte" Tiefe: depth = K(1,1)/(x1_pixel-x2_pixel)
+% Daraus folgt:
+% lambda1 = lambda2 = lambda = z-Komponente der Punkte im Raum
+%         = K(1,1)*depth_map'
+% Umformen von Gleichung(1) ergibt die neuen Pixelkoordinaten:
+% x2_pixel = x1_pixel + T_pixel/lambda
+% => x2_pixel = x1_pixel + (K(1,1)*T_x) / ((K(1,1))*depth_map')
+%             = x1_pixel + T_x / depth_map'
+NEW_pixel_x = L_pixel_x + T_x./depth_map';
+% T_y = 0 und T_z = 0, daraus folgt, dass die y- und z-Pixelkoordinaten
+% unverändert bleiben. (z-Komponente hat sowohl in homogenen
+% Bildkoordinaten als auch homogenen Pixelkoordinaten den Wert 1.)
+
 % Valide Pixel bestimmen
-p_valid = isfinite(NEW_pixel_x) & isfinite(NEW_pixel_y);
+p_valid = isfinite(NEW_pixel_x);
 NEW_pixel_x = NEW_pixel_x(p_valid);
-NEW_pixel_y = NEW_pixel_y(p_valid);
+NEW_pixel_y = L_pixel_y(p_valid);
 v = double(I1_rec(p_valid));
 
 F = scatteredInterpolant(NEW_pixel_x(:),NEW_pixel_y(:),v);
 fprintf('F berechnet')
-new_rectified_img = uint8(F({1:size(I1_rec,2),1:size(I1_rec,1)}))';
-
+new_rectified_img = uint8(F({1:max(NEW_pixel_x),1:size(I1_rec,1)}))';
 end
-
