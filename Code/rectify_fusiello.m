b033fc8fa124d53941661315228fe3eb95aeefd7
andrethom <41125321+andrethom@users.noreply.github.com>
2018-08-26 15:52:32 +0200

    Rrect zurückgeben

--- Code/rectify_fusiello.m
+++ Code/rectify_fusiello.m
@@ -1,24 +1,25 @@
-function [TL,TR] = rectify_fusiello(PoL,PoR,K,dL,dR)
+function [TL,TR,R_rect] = rectify_fusiello(PoL,PoR,K,R_rect,dL,dR)
 %RECTIFY_FUSIELLO Summary of this function goes here
 %   Detailed explanation goes here
 
 if nargin == 3
     dL = [0;0];
     dR = [0;0];
+    
+    c1 = -inv(PoL(:,1:3))*PoL(:,4); % is equal to (0,0,0)
+    % Epipol e1
+    c2 = -inv(PoR(:,1:3))*PoR(:,4); % is equal to -R'*T
+
+    v1 = (c2-c1); % new x-axis along translation vector
+    v2 = cross([0 0 1]',v1); % new y-axis is orthogonal to x- and z-axis
+    v3 = cross(v1,v2); % z-axis is crossproduct of x-/
+
+    % new extrinsic (translation unchanged)
+    R_rect = [v1'/norm(v1)
+              v2'/norm(v2)
+              v3'/norm(v3)];
 end
 
-c1 = -inv(PoL(:,1:3))*PoL(:,4); % is equal to (0,0,0)
-% Epipol e1
-c2 = -inv(PoR(:,1:3))*PoR(:,4); % is equal to -R'*T
-
-v1 = (c2-c1); % new x-axis along translation vector
-v2 = cross([0 0 1]',v1); % new y-axis is orthogonal to x- and z-axis
-v3 = cross(v1,v2); % z-axis is crossproduct of x-/
-
-% new extrinsic (translation unchanged)
-R_rect = [v1'/norm(v1)
-          v2'/norm(v2)
-          v3'/norm(v3)];
 
 % translate image centers
 KL = K;
@@ -43,7 +44,7 @@ KR(2,3)=KR(2,3)+dR(2);
 % Einsetzen der Gleichung für die Home Position in die Gleichungen für die
 % rotierten Pixelkoordinaten, führt zu:
 % Linkes Bild: x' = KL*R_rect*inv(K)*x
-% Rechtes Bild: x' = KR*R*_rect*R'*inv(K)*x
+% Rechtes Bild: x' = KR*R_rect*R'*inv(K)*x
 % Daraus ergeben sich die folgenden Homographien:
 % TL = KL*R_rect*inv(K)
 % TR = KR*R_rect*inv(K*R) = KR*R_rect*inv(R)*inv(K)
