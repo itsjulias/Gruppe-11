a62ecaee612959c10522aaeffcc7c88373170b35
andrethom <41125321+andrethom@users.noreply.github.com>
2018-08-28 21:13:47 +0200

    mittelwert ransac, andere parameter

--- Code/free_viewpoint.m
+++ Code/free_viewpoint.m
@@ -18,66 +18,80 @@ IGray2 = gain_offset_correction_cdf(IGray2);
 IGray1 = uint8(bfltGray(double(IGray1),12,100,15));
 IGray2 = uint8(bfltGray(double(IGray2),12,100,15));
 
+% save('zw_nach_filter')
+% load('zw_nach_filter')
+
 %% Harris-Merkmale berechnen
 disp('-------------getting feature points--------------')
 % Robuste Einstellungen 'segment_length',15,'k',0.12,'min_dist',10,'N',20
-Merkmale1 = harris_detektor(IGray1,'segment_length',15,'k',0.15,'min_dist',20,'N',40,'do_plot',devMode);
-Merkmale2 = harris_detektor(IGray2,'segment_length',15,'k',0.15,'min_dist',20,'N',40,'do_plot',devMode);
+Merkmale1 = harris_detektor(IGray1,'segment_length',15,'k',0.15,'min_dist',40,'N',5,'do_plot',devMode);
+Merkmale2 = harris_detektor(IGray2,'segment_length',15,'k',0.15,'min_dist',40,'N',5,'do_plot',devMode);
+
 
 %% Korrespondenzschaetzung
 disp('-------------estimation correspondence points--------------')
 % Robuste Einstellungen 'window_length',55,'min_corr',0.9
 Korrespondenzen = punkt_korrespondenzen(IGray1,IGray2,Merkmale1,Merkmale2,'window_length',45,'min_corr',0.95,'do_plot',devMode);
 
-%% Finde robuste Korrespondenzpunktpaare mit Hilfe des RANSAC-Algorithmus
-disp('-------------finding robust correspondence points--------------')
-Korrespondenzen_robust = F_ransac(Korrespondenzen,'epsilon',0.7, 'tolerance', 0.01);
+T_sum = zeros(3,1);
+R_sum = zeros(3,3);
+for i = 1:10
+        %% Finde robuste Korrespondenzpunktpaare mit Hilfe des RANSAC-Algorithmus
+        disp('-------------finding robust correspondence points--------------')
+        Korrespondenzen_robust = F_ransac(Korrespondenzen,'epsilon',0.5,'p',0.999,'tolerance', 0.1);
 
-% Zeige die robusten Korrespondenzpunktpaare
-if(devMode)
-    disp('-------------plot robust correspondence points--------------')
-    figure
-    im1 = imshow(IGray1);
-    hold all;
-    im2 = imshow(IGray2);
-    im1.AlphaData = 0.5;
-    im2.AlphaData = 0.5;
-    plot(Korrespondenzen_robust(1,:),Korrespondenzen_robust(2,:),'*r');
-    plot(Korrespondenzen_robust(3,:),Korrespondenzen_robust(4,:),'*g');
-    for i = 1:length(Korrespondenzen_robust(1,:))
-        plot([Korrespondenzen_robust(1,i) Korrespondenzen_robust(3,i)],[Korrespondenzen_robust(2,i) Korrespondenzen_robust(4,i)],'b');
+    % Zeige die robusten Korrespondenzpunktpaare
+    if(devMode)
+        disp('-------------plot robust correspondence points--------------')
+        figure
+        im1 = imshow(IGray1);
+        hold all;
+        im2 = imshow(IGray2);
+        im1.AlphaData = 0.5;
+        im2.AlphaData = 0.5;
+        plot(Korrespondenzen_robust(1,:),Korrespondenzen_robust(2,:),'*r');
+        plot(Korrespondenzen_robust(3,:),Korrespondenzen_robust(4,:),'*g');
+        for i = 1:length(Korrespondenzen_robust(1,:))
+            plot([Korrespondenzen_robust(1,i) Korrespondenzen_robust(3,i)],[Korrespondenzen_robust(2,i) Korrespondenzen_robust(4,i)],'b');
+        end
     end
+
+    %% Berechne die Essentielle Matrix
+    % Kamerakalibrierungsmatrix ist in KK.mat enthalten und wurde mit in der
+    % Video-Vorlesung angegebenen Toolbox bestimmt.
+    disp('-------------essential matrix calculation--------------')
+
+    load('calib_K.mat');
+
+    E = achtpunktalgorithmus(Korrespondenzen_robust,K);
+    % F = achtpunktalgorithmus(Korrespondenzen_robust);
+    % disp(E);
+    % disp(F);
+    [T1, R1, T2, R2, U, V] = TR_aus_E(E);
+
+    [T_cell, R_cell,T,R, d_cell, x1, x2] = ...
+        rekonstruktion(T1, T2, R1, R2, Korrespondenzen_robust, K);
+    
+   T_sum = T_sum + T;
+   R_sum = R_sum + R;
 end
+T = T_sum/10;
+R = R_sum/10;
 
-%% Berechne die Essentielle Matrix
-% Kamerakalibrierungsmatrix ist in KK.mat enthalten und wurde mit in der
-% Video-Vorlesung angegebenen Toolbox bestimmt.
-disp('-------------essential matrix calculation--------------')
 
-load('calib_K.mat');
-
-E = achtpunktalgorithmus(Korrespondenzen_robust,K);
-% F = achtpunktalgorithmus(Korrespondenzen_robust);
-% disp(E);
-% disp(F);
-[T1, R1, T2, R2, U, V] = TR_aus_E(E);
-
-[T_cell, R_cell,T,R, d_cell, x1, x2] = ...
-    rekonstruktion(T1, T2, R1, R2, Korrespondenzen_robust, K);
-
-% save('zw_for_rec_27_08');
 % load('zw_for_rec_27_08');
-% p = 1;
 
 %% Bildrektifizierungsalgorithmus
 disp('-------------rectification--------------')
 [img1_rectified,img2_rectified,Tr1,Tr2,R_rect,offset_x_pixel,...
     img1_rectified_full,d_cut_up,d_cut_down] = ...
     rectification(IGray1,IGray2,K,T,R,'do_plot',devMode,'size_frame','valid_offset');
-figure
-imshow(img1_rectified_full)
-% save('zw_R_rect','R_rect');
-
+% figure
+% imshow(img1_rectified_full)
+% % save('zw_R_rect','R_rect');
+% % save('comp_good')
+% save('comp_28_08')
+% load('comp_28_08');
 
 %% Disparitätsermittling
 disp('---------disparity estimation-----------')
@@ -93,12 +107,12 @@ alpha = p*get_max_alpha(R);
                                                   K,-p,R_rect,alpha,...
                                       offset_x_pixel, d_cut_up,d_cut_down);
 
-% figure
-% imshow(virtual_view_img);
+% save('res_28_08_V3_try2')
+
 figure
 imshow(img_rectified_new);
 %% Ausgabe des Free-Viewpoint Bildes
-output_image = uint8(p*image1+(1-p)*image2);
+% output_image = uint8(p*image1+(1-p)*image2);
 output_image = virtual_view_img;
 end
 
